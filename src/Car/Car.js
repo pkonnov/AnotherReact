042cf673a4d2e72d534bf2e49f255c0d0bc35933
Pavel Konnov <4itonada@gmail.com>
2018-10-11 12:47:47 +0400

    passing parameters to a function

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -5,6 +5,6 @@ export default (props) => (
   <div>
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
-    {props.children}
+    <button onClick={props.onChangeTitle}>Click</button>
   </div>
 )
